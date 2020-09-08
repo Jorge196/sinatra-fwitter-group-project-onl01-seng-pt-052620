2af3c43cf963b94e9f8a1f548ef424cee02eca57
Jorge Ramirez <joramirez96@gmail.com>
2020-09-08 23:21:16 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,32 @@ class TweetsController < ApplicationController
     enable :sessions
     set :session_secret, "secret"
   end
-  
+
+  get '/tweets' do
+    if logged_in?
+      @users = current_user
+      @tweets = Tweet.all
+      erb :'/tweets/tweets'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+      @user = current_user
+      erb :'tweets/create_tweet'
+    else
+      redirect "/login"
+    end
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.new(params)
+    @user = current_user
+
+    if logged_in? && !@tweet.content.blank? && @tweet.save
+      @user.tweets << @tweet
 
 
 end
